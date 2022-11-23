74bb6521a378b3799ec9d921c7f27916897503df
Adiel Eichenstein <adiel.eich@gmail.com>
2022-11-23 12:11:04 -0500

    new links styled + added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+
+const button1 = document.querySelector('#instagram');
+
+
+button1.addEventListener('mouseover', (event) => {    
+  /*event.preventDefault()
+ console.log("hello")*/
+ button1.style.color = 'pink';
+});
+
+
+const button2 = document.querySelector('#pinterest');
+button2.addEventListener('mouseover', (event) => {    
+    button2.style.color = 'pink';
+});
+
+
+
+
+/*document.addEventListener('scroll', (event) => {});*/
+
