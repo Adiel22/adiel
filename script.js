864e7106d9731557289d8af6ee7ae0f55bef5f12
Adiel Eichenstein <adiel.eich@gmail.com>
2022-11-23 12:20:49 -0500

    button color change

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ const button1 = document.querySelector('#instagram');
 
 
 button1.addEventListener('mouseover', (event) => {    
-  /*event.preventDefault()
- console.log("hello")*/
- button1.style.color = 'pink';
+ 
 });
 
 
 const button2 = document.querySelector('#pinterest');
+
 button2.addEventListener('mouseover', (event) => {    
-    button2.style.color = 'pink';
+
 });
 
 
@@ -20,3 +19,5 @@ button2.addEventListener('mouseover', (event) => {
 
 /*document.addEventListener('scroll', (event) => {});*/
 
+ /*event.preventDefault()
+ console.log("hello")*/
